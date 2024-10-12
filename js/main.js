fc391d905914677a7aef61647a2ac835bd5f817f
_.mindang <minhdang875425@gmail.com>
2024-10-12 17:42:33 +0700

    best slider

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@
 let list = document.querySelector(".slider .slider__list");
 let items = document.querySelectorAll(".slider .slider__list .slider__item");
 let dots = document.querySelectorAll(".slider .slider__dots li");
-let prev = document.getElementById("slider__prev");
 let next = document.getElementById("slider__next");
+let prev = document.getElementById("slider__prev");
 
 let active = 0;
 let lengthItems = items.length - 1;
@@ -47,6 +47,47 @@ dots.forEach((li, key) =>{
         ReloadSlider();
     })
 })
+
+//slider
+
+let best_list = document.querySelector(".best__slider .best__slider__list");
+let best_items = document.querySelectorAll(".best__slider .best__slider__list .best__slider__item");
+// let best_dots = document.querySelectorAll(".best__slider .best__slider__dots li");
+let best_next = document.getElementById("best__slider__next");
+let best_prev = document.getElementById("best__slider__prev");
+
+let best_active = 0;
+let best_lengthItems = best_items.length - 1;
+
+best_next.onclick = function()
+{
+    if(best_active + 1 > best_lengthItems)
+        best_active = 0;
+    else
+        best_active++;
+    best_ReloadSlider();
+}
+
+best_prev.onclick = function()
+{
+    if(best_active - 1 < 0)
+        best_active = best_lengthItems;
+    else
+        best_active--;
+    best_ReloadSlider();
+}
+
+
+function best_ReloadSlider()
+{
+    let best_checkLeft = best_items[best_active].offsetLeft;
+    best_list.style.left = -best_checkLeft + "px";
+    // best_list.style.left = -330 + "px";
+
+    // let lastActiveDot = document.querySelector(".slider .slider__dots li.active");
+    // lastActiveDot.classList.remove("active");
+    // dots[active].classList.add("active");
+}
 
 
 
