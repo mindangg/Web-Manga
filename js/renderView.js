d3958cd55e80b8c7ea5574b2da00e6dbd8d074e6
_.mindang <minhdang875425@gmail.com>
2024-11-06 21:02:20 +0700

    fixed best slider js

--- js/renderView.js
+++ js/renderView.js
@@ -1,4 +1,134 @@
+var bestSellingProduct = [
+    {
+        productId: "manga_1", 
+        series: "Sakamoto Days", 
+        author: "Suzuki Yuto", 
+        category: "Action", 
+        img1: "../img/books/sakamoto days/sakamoto-days-volume-6-primary.jpg", 
+        img2: "../img/books/sakamoto days/sakamoto-days-volume-6-back.jpg", 
+        name: "Sakamoto Days - Volume 06", 
+        price: 9.99, 
+        stock: 120,
+        descr1: "Sakamoto Days manga volume 6 features story and art by Yuto Suzuki.",
+        descr2: "Dangerous serial killers clash with the Order and Sakamoto’s crew! As the battles rage on, Sakamoto undergoes an unexpected transformation. Meanwhile, the evil mastermind X reveals his true intent, which sends shock waves through the assassin world!",
+    },
 
+    {
+        productId: "manga_2", 
+        series: "Jujutsu Kaisen", 
+        author: "Gege Akutami", 
+        category: "Action", 
+        img1: "../img/books/jujutsu kaisen/jujutsu-kaisen-volume-4-primary.jpg", 
+        img2: "../img/books/jujutsu kaisen/jujutsu-kaisen-volume-4-back.jpg", 
+        name: "Jujutsu Kaisen - Volume 04", 
+        price: 7.99, 
+        stock: 90,
+        descr1: "Jujutsu Kaisen manga volume 14 features story and art by Gege Akutami.",
+        descr2: "While investigating a strange set of mysterious deaths, Itadori meets Junpei, a troubled kid who is often bullied at school, and they immediately hit it off. However, Junpei is also befriended by the culprit behind the bloody incidents—Mahito, a mischievous cursed spirit! Mahito sets in motion a devious plan involving Junpei, hoping to ensnare Itadori as well.",
+    },
+
+    {
+        productId:"manga_3", 
+        series: "Spy X Family", 
+        author: "Tatsuya Endo", 
+        category: "Comedy", 
+        img1: "../img/books/spy x family/spy-x-family-volume-3-primary.jpg", 
+        img2: "../img/books/spy x family/spy-x-family-volume-3-back.jpg", 
+        name: "Spy X Family - Volume 03", 
+        price: 7.99, 
+        stock: 100,
+        descr1: "Spy x Family manga volume 3 features story and art by Tatsuya Endo.",
+        descr2: "Twilight has overcome many challenges in putting together the Forger family, but now all his hard work might come undone when Yor’s younger brother Yuri pops in for a surprise visit! Can Twilight outsmart Yuri when he finds out Yuri is actually a mortal enemy of his intelligence agency—an Ostanian Secret Service officer?"
+    },
+
+
+    {
+        productId:"manga_4", 
+        series: "My Dress Up Darling", 
+        author: "Fukuda Shinichi", 
+        category: "Rom-Com", 
+        img1: "../img/books/my dress up darling/my-dress-up-darling-volume-7-primary.jpg", 
+        img2: "../img/books/my dress up darling/my-dress-up-darling-volume-7-back.jpg", 
+        name: "My Dress Up Darling - Volume 07", 
+        price: 9.99, 
+        stock: 150,
+        descr1: "My Dress-Up Darling manga volume 7 features story and art by Shinichi Fukuda.",
+        descr2: "Marin's bestie and Wakana's classmate Nowa might seem a little flaky, but the girl has some serious instinct when love is in the air! When she tosses out a bombshell at karaoke by flatly asking Marin and Wakana if they're an item, it sends the two running!"
+    },
+
+    {
+        productId:"manga_5", 
+        series: "Black Clover", 
+        author: "Yuki Tabata", 
+        category: "Shounen", 
+        img1: "../img/books/black clover/black-clover-volume-23-primary.jpg", 
+        img2: "../img/books/black clover/black-clover-volume-23-back.jpg", 
+        name: "Black Clover - Volume 23", 
+        price: 9.99, 
+        stock: 150,
+        descr1: "Black Clover manga volume 23 features story and art by Yuki Tabata.",
+        descr2: "With the devil finally defeated, the Magic Knights are working hard to rebuild the damage done to the Clover Kingdom. Unfortunately for Asta, all the blame is being pinned on him and he’s set to be executed. He’s in the most trouble he’s ever been in, but you never count out a Black Bull!"
+    },
+
+    {
+        productId:"manga_6", 
+        series: "My Hero Academia", 
+        author: "Yuki Tabata", 
+        category: "Shounen", 
+        img1: "../img/books/my hero academia/my-hero-academia-manga-volume-11-primary.jpg", 
+        img2: "../img/books/my hero academia/my-hero-academia-manga-volume-11-back.jpg", 
+        name: " Kohei Korikoshi - Volume 11", 
+        price: 7.99, 
+        stock: 150,
+        descr1: "My Hero Academia volume 11 features story and art by Kohei Korikoshi.",
+        descr2: "Bakugo’s abduction by the League of Villains was a carefully calculated move designed to draw out the Heroes—All Might in particular—and destroy them. Midoriya and his friends set out on a rescue mission that eventually pulls in not only All Might but also several other heroes! The casualties mount, until at last the mastermind of the plot appears—the only villain powerful enough to go head-to-head with All Might and possibly win…All For One!"
+    },
+
+    {
+        productId:"manga_7", 
+        series: "One Punch Man", 
+        author: "Yusuke Murata", 
+        category: "Shounen", 
+        img1: "../img/books/one punch man/one-punch-man-volume-26-primary.jpg", 
+        img2: "../img/books/one punch man/one-punch-man-volume-26-back.jpg", 
+        name: "One Punch Man - Volume 26", 
+        price: 7.99, 
+        stock: 150,
+        descr1: "One-Punch Man manga volume 26 features story by ONE and art by Yusuke Murata.",
+        descr2: "As the fight against the Monster Association big shots heats up, Class-S heroes find themselves locked in a desperate struggle, but Tornado is closing in on Gyoro-Gyoro’s main body! Meanwhile, Blackluster confronts Garo, while Saitama wanders the labyrinth where the Monster Association is hiding out."
+    },
+
+    {
+        productId:"manga_8", 
+        series: "Mashle: Magic And Muscles", 
+        author: "Hajime Komoto", 
+        category: "Comedy", 
+        img1: "../img/books/mashle/mashle-magic-and-muscles-manga-volume-17-primary.jpeg", 
+        img2: "../img/books/mashle/mashle-magic-and-muscles-manga-volume-17-back.jpeg", 
+        name: "Mashle: Magic And Muscles - Volume 17", 
+        price: 9.59, 
+        stock: 150,
+        descr1: "Mashle: Magic and Muscles manga volume 17 features story and art by Hajime Komoto.",
+        descr2: "Innocent Zero’s youngest child, Domina, steps in to put a stop to his father’s pursuit of Mash. Using the reprieve afforded them, Mash’s friends race against the clock in hopes of getting him to Meliadoul, the only hope he has of surviving without a heart. Meanwhile, as Mash hovers between life and death, he encounters a mysterious figure."
+    },
+
+]
+
+let i = 1
+bestSellingProduct.forEach(p => {
+    document.querySelector(".best__slider__list").innerHTML += `
+                <div class="best__slider__item">
+                    <a id="best__item${i}">
+                        <img src="${p.img1}" alt="">
+                        <img src="${p.img2}" alt="">
+                    </a>
+                    <h4>${p.name}</h4>
+                    <p>$${p.price}</p>
+                    <button>+ Add to cart</button>
+                </div>
+    `
+    i++
+})
 
 
 
@@ -38,47 +168,48 @@ document.getElementById("product__close").addEventListener("click", function(){
       }, 365);
 })
 
-// function showBestInfo(){
-//     const productInfo = document.querySelector(".product__page")
-//     productInfo.innerHTML = `
-//             <div class="product">
-//                 <a id="product__close"><i class="fa-solid fa-xmark" style="color:white;"></i></a>
-//                 <div class="product__img">
-//                     <img id="product__img1" src="${p.img1}">
-//                     <img id="product__img2" src="${p.img2}">
-
-//                     <a id="product__view1"><img src="${p.img1}"></a>
-
-//                     <a id="product__view2"><img src="${p.img2}"></a>
-
-//                 </div>
+function showBestInfo(){
+    const productInfo = document.querySelector(".product__page")
     
-//                 <div class="product__info">
-//                     <h1></h1>
-//                     <div class="product__info--rating">
-//                         <i class="fa-solid fa-star"></i>
-//                         <i class="fa-solid fa-star"></i>
-//                         <i class="fa-solid fa-star"></i>
-//                         <i class="fa-solid fa-star"></i>
-//                         <i class="fa-solid fa-star"></i>
-//                     </div>
-//                     <p>Author: ${p.author}</p>
-//                     <h2></h2>
-//                     <p></p>
-//                     <p>Quantity: </p>
+    productInfo.innerHTML = `
+            <div class="product">
+                <a id="product__close"><i class="fa-solid fa-xmark" style="color:white;"></i></a>
+                <div class="product__img">
+                    <img id="product__img1" src="${p.img1}">
+                    <img id="product__img2" src="${p.img2}">
+
+                    <a id="product__view1"><img src="${p.img1}"></a>
+
+                    <a id="product__view2"><img src="${p.img2}"></a>
+
+                </div>
     
-//                     <button id="product__quantitydown">-</button><input type="text" id="product__quantity" value="1"><button id="product__quantityup">+</button><br>
-//                     <button id="product__add">Add to cart</button>
+                <div class="product__info">
+                    <h1></h1>
+                    <div class="product__info--rating">
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                    </div>
+                    <p>Author: ${p.author}</p>
+                    <h2></h2>
+                    <p></p>
+                    <p>Quantity: </p>
     
-//                     <p>Availability: ${p.stock}</p><br>
-//                     <h4>Description</h4>
-//                     <p>${p.descr1}</p><br>
-//                     <p>${p.descr2}</p>
-//                 </div>
-//                 </div>
-//             </div>
-// `
-// }
+                    <button id="product__quantitydown">-</button><input type="text" id="product__quantity" value="1"><button id="product__quantityup">+</button><br>
+                    <button id="product__add">Add to cart</button>
+    
+                    <p>Availability: ${p.stock}</p><br>
+                    <h4>Description</h4>
+                    <p>${p.descr1}</p><br>
+                    <p>${p.descr2}</p>
+                </div>
+                </div>
+            </div>
+`
+}
 
 
 
